AOSP-Krypton/packages_apps_GamingMode

commit 4fd569a9115e7188291bc3405d25b6a10a64f322

GamingMode: Fix views obscuring other window that needs interaction
By default, WindowManager.LayoutParams() will goes MATCH_PARENT, thus
it'll block any other window that needs a touch event such as system
permission dialog.
But because we're also need that for hide event, I think the best way
to handle this is handling it all manually instead.

With this way, we can still be able to interact with other window while
still having the button overlay and danmaku enabled.

Issues: https://github.com/DerpFest-11/packages_apps_GamingMode/issues/1
Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

app/src/main/java/org/exthmui/game/services/OverlayService.java
@@ -204,6 +204,15 @@ public class OverlayService extends Service {
         // Danmaku Container visibility
         if (mDanmakuContainer != null) {
             final boolean showDanmaku = configBundle.getBoolean(Constants.ConfigKeys.SHOW_DANMAKU, Constants.ConfigDefaultValues.SHOW_DANMAKU);
+            WindowManager.LayoutParams danmakuParams = getBaseLayoutParams();
+            if (showDanmaku) {
+                danmakuParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+                danmakuParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+            } else {
+                danmakuParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+                danmakuParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+            }
+            mWindowManager.updateViewLayout(mDanmakuContainer, danmakuParams);
             mDanmakuContainer.setVisibility(showDanmaku ? View.VISIBLE : View.GONE);
         }
 
@@ -252,6 +261,8 @@ public class OverlayService extends Service {
             mGamingOverlayView = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.gaming_overlay_layout, null);
             mGamingOverlayView.setVisibility(View.GONE);
             WindowManager.LayoutParams mGamingViewLayoutParams = getBaseLayoutParams();
+            mGamingViewLayoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+            mGamingViewLayoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
             mWindowManager.addView(mGamingOverlayView, mGamingViewLayoutParams);
 
             mGamingMenu = mGamingOverlayView.findViewById(R.id.gaming_menu);
@@ -379,6 +390,10 @@ public class OverlayService extends Service {
 
         if (mGamingOverlayView.getVisibility() == View.VISIBLE && mode != 1) {
             // hide
+            WindowManager.LayoutParams menuLayoutParams = getBaseLayoutParams();
+            menuLayoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+            menuLayoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+            mWindowManager.updateViewLayout(mGamingOverlayView, menuLayoutParams);
             mGamingOverlayView.setVisibility(View.GONE);
             mGamingFloatingLayout.setVisibility(View.VISIBLE);
             hideFloatingButton(true);
@@ -396,6 +411,10 @@ public class OverlayService extends Service {
                 gravity |= Gravity.TOP;
             }
 
+            WindowManager.LayoutParams menuLayoutParams = getBaseLayoutParams();
+            menuLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+            menuLayoutParams.height = WindowManager.LayoutParams.MATCH_PARENT;
+            mWindowManager.updateViewLayout(mGamingOverlayView, menuLayoutParams);
             mGamingFloatingLayout.setVisibility(View.GONE);
             mGamingOverlayView.setGravity(gravity);
             ViewGroup.LayoutParams gamingMenuLayoutParams =  mGamingMenu.getLayoutParams();
@@ -411,6 +430,9 @@ public class OverlayService extends Service {
         if (mWindowManager != null && mDanmakuContainer == null) {
             mDanmakuContainer = new FrameLayout(this);
             WindowManager.LayoutParams danmakuParams = getBaseLayoutParams();
+            danmakuParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+            danmakuParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+            danmakuParams.gravity = Gravity.START | Gravity.TOP;
             danmakuParams.flags |= WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
             mWindowManager.addView(mDanmakuContainer, danmakuParams);
 

commit 4650a9812f2818548bfb7d9abf043acc52c5a5d2

GamingMode: Fix danmaku container layoutParam on config changed
On previous attempt at fixing obscured overlay event, I forget to also
restore other necessary flag for the danmaku container, which makes the
container to be placed right at the center instead of the top.

This patch should fix that behaviour and also making the code simpler.

Fixes: 4fd569a9115e ("GamingMode: Fix views obscuring other window that needs interaction")
Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

app/src/main/java/org/exthmui/game/services/OverlayService.java
@@ -78,6 +78,7 @@ public class OverlayService extends Service {
 
     private WindowManager mWindowManager;
     private WindowManager.LayoutParams mGamingFBLayoutParams;
+    private WindowManager.LayoutParams mDanmakuLayoutParams;
 
     private GamingPerformanceView performanceController;
 
@@ -204,16 +205,9 @@ public class OverlayService extends Service {
         // Danmaku Container visibility
         if (mDanmakuContainer != null) {
             final boolean showDanmaku = configBundle.getBoolean(Constants.ConfigKeys.SHOW_DANMAKU, Constants.ConfigDefaultValues.SHOW_DANMAKU);
-            WindowManager.LayoutParams danmakuParams = getBaseLayoutParams();
-            if (showDanmaku) {
-                danmakuParams.width = WindowManager.LayoutParams.MATCH_PARENT;
-                danmakuParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
-            } else {
-                danmakuParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
-                danmakuParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
-            }
-            mWindowManager.updateViewLayout(mDanmakuContainer, danmakuParams);
-            mDanmakuContainer.setVisibility(showDanmaku ? View.VISIBLE : View.GONE);
+            mDanmakuLayoutParams.width = showDanmaku ? WindowManager.LayoutParams.MATCH_PARENT : 0;
+            mDanmakuLayoutParams.height = showDanmaku ?  WindowManager.LayoutParams.WRAP_CONTENT : 0;
+            mWindowManager.updateViewLayout(mDanmakuContainer, mDanmakuLayoutParams);
         }
 
         if (mGamingMenu != null) {
@@ -429,12 +423,12 @@ public class OverlayService extends Service {
     private void initDanmaku() {
         if (mWindowManager != null && mDanmakuContainer == null) {
             mDanmakuContainer = new FrameLayout(this);
-            WindowManager.LayoutParams danmakuParams = getBaseLayoutParams();
-            danmakuParams.width = WindowManager.LayoutParams.MATCH_PARENT;
-            danmakuParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
-            danmakuParams.gravity = Gravity.START | Gravity.TOP;
-            danmakuParams.flags |= WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
-            mWindowManager.addView(mDanmakuContainer, danmakuParams);
+            mDanmakuLayoutParams = getBaseLayoutParams();
+            mDanmakuLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+            mDanmakuLayoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+            mDanmakuLayoutParams.gravity = Gravity.START | Gravity.TOP;
+            mDanmakuLayoutParams.flags |= WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
+            mWindowManager.addView(mDanmakuContainer, mDanmakuLayoutParams);
 
             mDanmakuManager = DanmakuManager.getInstance();
             mDanmakuManager.init(this, mDanmakuContainer);