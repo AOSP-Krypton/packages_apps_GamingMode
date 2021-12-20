096f628248f4cf0cfeedb93ffef457e83c70a0db
jhonboy121 <alfredmathew05@gmail.com>
2021-12-20 18:00:09 +0530

    GamingMode: initialize hilt di framework
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

---
+++ app/src/main/java/org/exthmui/game/GamingModeApp.kt
@@ -0,0 +1,8 @@
+package org.exthmui.game
+
+import android.app.Application
+
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp(Application::class)
+class GamingModeApp: Hilt_GamingModeApp()
