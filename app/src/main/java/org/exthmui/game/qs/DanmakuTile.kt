1f8f7c43fe7541bddee19b173d58c2b821ba878e
jhonboy121 <alfredmathew05@gmail.com>
2021-12-23 14:38:16 +0530

    GamingMode: DanmakuTile: java -> kt
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

---
+++ app/src/main/java/org/exthmui/game/qs/DanmakuTile.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2020 The exTHmUI Open Source Project
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.exthmui.game.qs
+
+import android.content.Context
+import android.provider.Settings
+
+import org.exthmui.game.R
+import org.exthmui.game.controller.DanmakuController
+
+class DanmakuTile(context: Context): TileBase(context, R.string.qs_danmaku, R.drawable.ic_qs_danmaku) {
+
+    private var danmakuController: DanmakuController? = null
+
+    init {
+        isSelected = Settings.System.getInt(context.contentResolver,
+            Settings.System.GAMING_MODE_SHOW_DANMAKU, 0) == 1
+    }
+
+    override fun handleClick(isSelected: Boolean) {
+        super.handleClick(isSelected)
+        danmakuController?.setShowDanmaku(isSelected)
+    }
+
+    fun setDanmakuController(controller: DanmakuController) {
+        danmakuController = controller
+    }
+}
